171e895ecf9b9f4cba5ef330e20591c5eca23af2
oskojess <jessica_osko@hotmail.com>
2020-08-05 18:11:51 -0300

    insert new test

--- Sum.test.js
+++ Sum.test.js
@@ -1,8 +1,13 @@
+
+const expect = require('chai').expect;
 const assert = require('assert');
 const getSoma = require('./Sum');
 
 describe("sum", function () {
   it("should be a simple sum", function () {
     assert(getSoma.somar(2, 2), 4);
-  })
+  });
+  it("deve receber apenas números", function () {
+    expect(() => getSoma.somar(null, undefined)).to.throw("expected NaN to be a function"); // FAILS
+  });
 })
